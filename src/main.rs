f154aa32a76197dff15b37f1631a4fd5ae8c7008
Fuyang Liu <liufuyang@users.noreply.github.com>
2019-03-29 12:50:24 +0100

    Use url to pass params

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,17 @@ extern crate serde_json;
 #[macro_use]
 extern crate fail;
 
-use futures::{future, Future, Stream};
+use futures::{future, Future};
 
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
 
-// use std::collections::HashMap;
+use std::collections::HashMap;
 // use url::form_urlencoded;
 
 static INDEX: &str = r#"Working. Try: curl localhost:8080/fail -XPOST -d'{"name": "index", "actions": "panic"}'"#;
-// static MISSING: &[u8] = b"Missing field";
-// static NOTNUMERIC: &[u8] = b"Number field is not numeric";
+static MISSING_NAME: &[u8] = b"Missing param name";
+static MISSING_ACTIONS: &[u8] = b"Missing param actions";
 
 #[derive(Deserialize, Debug)]
 struct FailPoint {
@@ -46,32 +46,50 @@ fn param_example(req: Request<Body>) -> Box<Future<Item=Response<Body>, Error=hy
             Box::new(future::ok(Response::new(INDEX.into())))
         },
         (&Method::PUT, "/fail") => {
-            Box::new(req.into_body().concat2().map(|b| {
-                let fail_point: FailPoint = match serde_json::from_slice(&b) {
-                    Ok(f) => f,
-                    Err(e) => return Response::builder()
-                        .status(StatusCode::UNPROCESSABLE_ENTITY)
-                        .body(e.to_string().into())
-                        .unwrap()
-                };
-                fail::cfg(fail_point.name.clone(), &fail_point.actions).unwrap();
-                let body = format!("Add fail point with name: {}, actions: {}", fail_point.name, fail_point.actions);
-                Response::new(body.into())
-            }))
+            let uri = &["http://base", &req.uri().to_string()].join("");
+            let params = url::Url::parse(uri).unwrap()
+                .query_pairs().into_owned().collect::<HashMap<String, String>>();
+
+            let name = if let Some(s) = params.get("name") {
+                s
+            } else {
+                return Box::new(future::ok(Response::builder()
+                    .status(StatusCode::UNPROCESSABLE_ENTITY)
+                    .body(MISSING_NAME.into())
+                    .unwrap()));
+            };
+
+            let actions = if let Some(s) = params.get("actions") {
+                s
+            } else {
+                return Box::new(future::ok(Response::builder()
+                    .status(StatusCode::UNPROCESSABLE_ENTITY)
+                    .body(MISSING_ACTIONS.into())
+                    .unwrap()));
+            };
+
+            fail::cfg(name.clone(), &actions).unwrap();
+            let body = format!("Add fail point with name: {}, actions: {}", name, actions);
+            Box::new(future::ok(Response::new(body.into())))
+
         },
         (&Method::DELETE, "/fail") => {
-            Box::new(req.into_body().concat2().map(|b| {
-                let fail_point: FailPointDelete = match serde_json::from_slice(&b) {
-                    Ok(f) => f,
-                    Err(e) => return Response::builder()
-                        .status(StatusCode::UNPROCESSABLE_ENTITY)
-                        .body(e.to_string().into())
-                        .unwrap()
-                };
-                fail::remove(&fail_point.name);
-                let body = format!("Delete fail point with name: {}", fail_point.name);
-                Response::new(body.into())
-            }))
+            let uri = &["http://base", &req.uri().to_string()].join("");
+            let params = url::Url::parse(uri).unwrap()
+                .query_pairs().into_owned().collect::<HashMap<String, String>>();
+
+            let name = if let Some(s) = params.get("name") {
+                s
+            } else {
+                return Box::new(future::ok(Response::builder()
+                    .status(StatusCode::UNPROCESSABLE_ENTITY)
+                    .body(MISSING_NAME.into())
+                    .unwrap()));
+            };
+
+            fail::remove(name);
+            let body = format!("Delete fail point with name: {}", name);
+            Box::new(future::ok(Response::new(body.into())))
         },
         (&Method::GET, "/fail") => {
             let list: Vec<String> = fail::list().into_iter().map(move|(s1, s2)| format!("{}: {}", s1, s2))
