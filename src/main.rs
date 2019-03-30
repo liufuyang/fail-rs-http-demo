2f732587957f6d76647741275fc7748b9342e4e2
Fuyang Liu <liufuyang@users.noreply.github.com>
2019-03-30 08:54:35 +0100

    Use pattern guards

--- src/main.rs
+++ src/main.rs
@@ -20,71 +20,7 @@ static FAIL_POINTS_PATH: &str = "/failpoints/";
 fn param_example(req: Request<Body>) -> Box<dyn Future<Item=Response<Body>, Error=hyper::Error> + Send> {
     let path = req.uri().path().to_owned();
     let method = req.method().to_owned();
-
-    // For paths start with /failpoints/
-    if path.starts_with(FAIL_POINTS_PATH) {
-        return match method {
-            Method::PUT => {
-                Box::new(req.into_body().concat2().map(move |chunk| {
-                    let (_, name) = path.split_at(FAIL_POINTS_PATH.len());
-                    if name.is_empty() {
-                        return Response::builder()
-                            .status(StatusCode::UNPROCESSABLE_ENTITY)
-                            .body(MISSING_NAME.into())
-                            .unwrap();
-                    };
-
-                    let actions = chunk.iter().cloned().collect::<Vec<u8>>();
-                    let actions = String::from_utf8(actions).unwrap();
-                    if actions.is_empty() {
-                        return Response::builder()
-                            .status(StatusCode::UNPROCESSABLE_ENTITY)
-                            .body(MISSING_ACTIONS.into())
-                            .unwrap();
-                    };
-
-                    if let Err(e) = fail::cfg(name.to_owned(), &actions) {
-                        return Response::builder()
-                            .status(StatusCode::BAD_REQUEST)
-                            .body(e.to_string().into())
-                            .unwrap();
-                    }
-                    let body = format!("Add fail point with name: {}, actions: {}", name, actions);
-                    Response::new(body.into())
-                }))
-            },
-            Method::DELETE => {
-                let (_, name) = path.split_at(FAIL_POINTS_PATH.len());
-                if name.is_empty() {
-                    return Box::new(future::ok(Response::builder()
-                        .status(StatusCode::UNPROCESSABLE_ENTITY)
-                        .body(MISSING_NAME.into())
-                        .unwrap()));
-                };
-
-                fail::remove(name);
-                let body = format!("Delete fail point with name: {}", name);
-                Box::new(future::ok(Response::new(body.into())))
-            },
-            Method::GET => {
-                let list: Vec<String> = fail::list().into_iter()
-                    .map(move |(s1, s2)| format!("{}={}", s1, s2))
-                    .collect();
-
-                let list = list.join("\n");
-                Box::new(future::ok(Response::builder()
-                    .status(StatusCode::UNPROCESSABLE_ENTITY)
-                    .body(list.into())
-                    .unwrap()))
-            },
-            _ =>  {
-                Box::new(future::ok(Response::builder()
-                    .status(StatusCode::METHOD_NOT_ALLOWED)
-                    .body(Body::empty())
-                    .unwrap()))
-            }
-        }
-    }
+    let is_on_failpoints: bool = path.starts_with(FAIL_POINTS_PATH);
 
     match (method, path.as_ref()) {
         (Method::GET, "/") => {
@@ -95,7 +31,49 @@ fn param_example(req: Request<Body>) -> Box<dyn Future<Item=Response<Body>, Erro
             fail_point!("home");
             Box::new(future::ok(Response::new(INDEX.into())))
         },
-        (Method::GET, "/failpoints") => {
+        (Method::PUT, _) if is_on_failpoints => {
+            Box::new(req.into_body().concat2().map(move |chunk| {
+                let (_, name) = path.split_at(FAIL_POINTS_PATH.len());
+                if name.is_empty() {
+                    return Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(MISSING_NAME.into())
+                        .unwrap();
+                };
+
+                let actions = chunk.iter().cloned().collect::<Vec<u8>>();
+                let actions = String::from_utf8(actions).unwrap();
+                if actions.is_empty() {
+                    return Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(MISSING_ACTIONS.into())
+                        .unwrap();
+                };
+
+                if let Err(e) = fail::cfg(name.to_owned(), &actions) {
+                    return Response::builder()
+                        .status(StatusCode::BAD_REQUEST)
+                        .body(e.to_string().into())
+                        .unwrap();
+                }
+                let body = format!("Add fail point with name: {}, actions: {}", name, actions);
+                Response::new(body.into())
+            }))
+        },
+        (Method::DELETE, _) if is_on_failpoints => {
+            let (_, name) = path.split_at(FAIL_POINTS_PATH.len());
+            if name.is_empty() {
+                return Box::new(future::ok(Response::builder()
+                    .status(StatusCode::UNPROCESSABLE_ENTITY)
+                    .body(MISSING_NAME.into())
+                    .unwrap()));
+            };
+
+            fail::remove(name);
+            let body = format!("Delete fail point with name: {}", name);
+            Box::new(future::ok(Response::new(body.into())))
+        },
+        (Method::GET, _) if is_on_failpoints || path.starts_with("/failpoints")=> {
             let list: Vec<String> = fail::list().into_iter()
                 .map(move |(s1, s2)| format!("{}={}", s1, s2))
                 .collect();
